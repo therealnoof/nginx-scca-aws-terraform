48933208686ace4c2d2f05f737945bd76d64ef71
therealnoof <hd1912@gmail.com>
2023-12-12 16:31:27 -0500

    Update transitgateway.tf

--- active-active-single-region/transitgateway.tf
+++ active-active-single-region/transitgateway.tf
@@ -1,5 +1,5 @@
 ## Create the TGW
-resource "aws_ec2_transit_gateway" "sslo" {
+resource "aws_ec2_transit_gateway" "nginx-scca" {
   description = "Transit Gateway for SSLO"
   tags = {
     Name = "${var.prefix}-tgw_sslo"
@@ -10,7 +10,7 @@ resource "aws_ec2_transit_gateway" "sslo" {
 ## Create the TGW Attachments
 resource "aws_ec2_transit_gateway_vpc_attachment" "securitystack" {
   subnet_ids             = [aws_subnet.internal.id]
-  transit_gateway_id     = aws_ec2_transit_gateway.sslo.id
+  transit_gateway_id     = aws_ec2_transit_gateway.nginx-scca.id
   vpc_id                 = aws_vpc.securitystack.id
   appliance_mode_support = "enable"
   tags = {
@@ -20,8 +20,8 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "securitystack" {
 
 resource "aws_ec2_transit_gateway_vpc_attachment" "appstack" {
   subnet_ids             = [aws_subnet.application.id]
-  transit_gateway_id     = aws_ec2_transit_gateway.sslo.id
-  vpc_id                 = aws_vpc.appstack.id
+  transit_gateway_id     = aws_ec2_transit_gateway.nginx-scca.id
+  vpc_id                 = aws_vpc.app stack.id
   appliance_mode_support = "enable"
   tags = {
     Name = "${var.prefix}-tgw_attachment_appstack"
@@ -33,14 +33,14 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "appstack" {
 resource "aws_ec2_transit_gateway_route" "return_public_ip" {
   destination_cidr_block         = "0.0.0.0/0"
   transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.securitystack.id
-  transit_gateway_route_table_id = aws_ec2_transit_gateway.sslo.association_default_route_table_id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway.nginx-scca.association_default_route_table_id
 }
 
 
 ## Create the TGW Route Table
-resource "aws_ec2_transit_gateway_route_table" "sslo" {
-  transit_gateway_id = aws_ec2_transit_gateway.sslo.id
+resource "aws_ec2_transit_gateway_route_table" "nginx-scca" {
+  transit_gateway_id = aws_ec2_transit_gateway.nginx-scca.id
   tags = {
-    Name = "${var.prefix}-rt_tgw_sslo"
+    Name = "${var.prefix}-rt_tgw_nginx-scca"
   }
 }
