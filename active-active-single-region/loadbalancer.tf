bf246725be731300cd4cd5dbec0077ed69f0a9fd
noof <hd1912@gmail.com>
2023-12-14 16:12:11 -0800

    updates

--- active-active-single-region/loadbalancer.tf
+++ active-active-single-region/loadbalancer.tf
@@ -65,7 +65,8 @@ resource "aws_lb_target_group" "ngninx_stack_80 {
   }
 
   targets = [ 
-    ""
+    "aws_network_interface.nginx_external_top_tier_az1.id"
+    "aws_network_interface.nginx_external_top_tier_az2.id"
   ]
 }
 
